8b5005e9041a48cb61b0478ad48302971a63d6d0
romanjrdykyj <romanjrdykyj@gmail.com>
2020-04-01 19:04:19 +0200

    actionbar(bez kolorów)
    trzeba ustawic kolor paska zadan (tam gdzie godzina, bateria itp.) oraz zmienic czcionke na lepszą

--- app/src/main/java/com/example/edenappbeta/MainActivity.java
+++ app/src/main/java/com/example/edenappbeta/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.edenappbeta;
 
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+
 }
